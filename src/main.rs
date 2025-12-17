0262697e3f498b287f341b382fcaaf7878c0c2af
zack kourouma <zack@kourouma.me>
2025-12-17 13:48:31 -0300

    feat: Add command not found error

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::{
+    env::args,
+    io::{self, Write},
+};
 
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut args = args();
+    if let Some(cmd) = args.nth(0) {
+        print!("Command not found: {cmd}");
+    }
 }
