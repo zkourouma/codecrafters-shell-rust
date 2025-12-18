fec2b7bf31b6174c25f6ae08f785418ea3b3d20e
zack kourouma <zack@kourouma.me>
2025-12-18 19:08:15 -0300

    feat: Infinitely looping repl

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use std::io::{self, Write, stdin};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut input = String::new();
+        let mut input = String::new();
 
-    stdin()
-        .read_line(&mut input)
-        .expect("Unable to read user input");
-    let cmd = input.trim();
-    if !cmd.is_empty() {
-        print!("{cmd}: command not found");
+        stdin()
+            .read_line(&mut input)
+            .expect("Unable to read user input");
+        let cmd = input.trim();
+        if !cmd.is_empty() {
+            print!("{cmd}: command not found");
+        }
     }
 }
