9a362c923f7b35850bfc80e3db44d6463ac4ad09
zack kourouma <zack@kourouma.me>
2025-12-22 16:45:50 -0300

    feat: Add PATH traversal to find commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::io::{self, Write, stdin};
+use std::{
+    env,
+    fs::read_dir,
+    io::{self, Write, stdin},
+    os::unix::fs::PermissionsExt,
+    path::PathBuf,
+};
 
 const BUILTINS: &[&str; 3] = &["exit", "echo", "type"];
 
@@ -18,7 +24,7 @@ fn main() {
         match cmd {
             "exit" => break,
             "echo" => println!("{}", args.join(" ")),
-            "type" => is_type(args),
+            "type" => has_type(args),
             _ => println!("{cmd}: command not found"),
         }
     }
@@ -34,14 +40,40 @@ fn parse_cmd<'a>(input: &'a str) -> (&'a str, Vec<&'a str>) {
     }
 }
 
-fn is_type(cmds: Vec<&str>) {
+fn has_type(cmds: Vec<&str>) {
     if cmds.len() < 1 {
         println!("command not found");
     } else if BUILTINS.contains(&cmds[0]) {
         let cmd = cmds[0];
         println!("{cmd} is a shell builtin");
+    } else if let Some(executable) = find_in_path(cmds[0]) {
+        let cmd = cmds[0];
+        let exec_path = executable.to_str().unwrap_or_default();
+        println!("{cmd} is {exec_path}");
     } else {
         let cmd = cmds[0];
         println!("{cmd}: not found");
     }
+}
+
+fn find_in_path(cmd: &str) -> Option<PathBuf> {
+    let path_cmd = format!("{cmd}");
+    env::var_os("PATH")
+        .and_then(|path_var| path_var.into_string().ok())
+        .and_then(|path| {
+            for dir in path.split(":") {
+                if let Ok(dir_entries) = read_dir(dir) {
+                    for dir_entry in dir_entries.flat_map(|d| d) {
+                        if dir_entry.path().ends_with(&path_cmd) {
+                            if let Ok(metadata) = dir_entry.metadata() {
+                                if metadata.permissions().mode() & 0x111 != 0 {
+                                    return Some(dir_entry.path());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            None
+        })
 }
